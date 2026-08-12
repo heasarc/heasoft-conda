76c559d38dffd748d8d812fa53aecce9f2d4f1f2
Abdu Zoghbi <astrozoghbi@gmail.com>
2026-08-12 14:22:16 -0400

    add fviewer recipe and make it a depenendy in heasoft

--- heasoft/build.sh
+++ heasoft/build.sh
@@ -110,8 +110,6 @@ fi
 # modify the rpath, etc. (e.g. libxpa.so.1.0)
 find $PREFIX/$HEA_SUBDIR/lib -type f ! -type l -name "*$SHLIB_EXT*" -exec chmod 755 {} \;
 
-# Install fviewer:
-#$PYTHON -m pip install fviewer
 
 # write initialization scripts
 # 1. write them to bin/heainit.[c]sh
